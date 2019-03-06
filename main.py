5077bd0845570517814016b7db9ec0f8b34f9829
Thorn <thorn.prive@gmail.com>
2019-03-06 19:29:56 +0100

    Reading arguments from command line + scapy libary

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-...
+import sys
+import scapy.all as scapy
+
+if __name__ == "__main__":
+    arguments = sys.argv
+    ips = []
+
+    for ip in arguments[1:]:
+        ips.append(ip)
+
+    print(ips)
+
